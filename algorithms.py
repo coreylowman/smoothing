e0e8d6ac05999dedace15ffca5244c511a7f122e
Corey Lowman <clowman1993@gmail.com>
2018-10-20 13:02:46 -0400

    Adding differential evoluation

--- algorithms.py
+++ algorithms.py
@@ -1,12 +1,13 @@
-from typing import Callable, List, TypeVar, Dict, Hashable, Iterable, Sized
+from typing import Callable, List, Tuple, Dict
+import random
 
-Individual = TypeVar('T', Hashable, Iterable, Sized)
+Individual = Tuple[float]
 Population = List[Individual]
 PopulationFitness = Dict[Individual, float]
 
 
 def genetic_algorithm(
-        num_generations,
+        terminate_fn: Callable[[Population, Population], bool],
         initialize_fn: Callable[[], Population],
         fitness_fn: Callable[[Individual], float],
         select_fn: Callable[[Population, PopulationFitness], Population],
@@ -18,18 +19,70 @@ def genetic_algorithm(
     population_size = len(population)
     fitness_by_individual = {individual: fitness_fn(individual) for individual in population}
 
-    for generation in range(num_generations):
-        new_population = []
-        while len(new_population) != population_size:
+    while True:
+        all_children = []
+        while len(all_children) != population_size:
             parents = select_fn(population, fitness_by_individual)
             children = crossover_fn(parents)
             for child in children:
                 child = mutation_fn(child)
                 fitness_by_individual[child] = fitness_fn(child)
-                new_population.append(child)
+                all_children.append(child)
 
-        population = replace_fn(population, new_population, fitness_by_individual)
-        fitness_by_individual = {individual: fitness_by_individual[individual] for individual in population}
-        print(generation, fitness_by_individual)
+        new_population = replace_fn(population, all_children, fitness_by_individual)
+        fitness_by_individual = {individual: fitness_by_individual[individual] for individual in new_population}
+
+        old_population = population
+        population = new_population
+
+        if terminate_fn(old_population, population):
+            break
 
     return population
+
+
+def differential_evolution(
+        terminate_fn: Callable[[Population, Population], bool],
+        initialize_fn: Callable[[], Population],
+        fitness_fn: Callable[[Individual], float],
+        fitness_is_better_fn: Callable[[float, float], bool],
+        crossover_rate: float, differential_weight: float
+) -> Population:
+    population = initialize_fn()
+    fitness_by_individual = {individual: fitness_fn(individual) for individual in population}
+
+    while True:
+        new_population = []
+        for i, individual in enumerate(population):
+            other_individuals = list(range(len(population)))
+            other_individuals.remove(i)
+            a, b, c = random.sample(other_individuals, 3)
+
+            new_individual = list(individual)
+            j_rand = random.randrange(len(individual))
+            for j in range(len(individual)):
+                mutated = a[j] + differential_weight * (b[j] - c[c])
+                trial = mutated if random.uniform(0, 1) <= crossover_rate or j == j_rand else individual[j]
+                new_individual[j] = trial
+            new_individual = type(individual)(new_individual)
+            fitness_by_individual[new_individual] = fitness_fn(new_individual)
+
+            if fitness_is_better_fn(fitness_by_individual[new_individual], fitness_by_individual[individual]):
+                new_population.append(new_individual)
+            else:
+                new_population.append(individual)
+
+        fitness_by_individual = {individual: fitness_by_individual[individual] for individual in new_population}
+
+        old_population = population
+        population = new_population
+
+        if terminate_fn(old_population, population):
+            break
+
+    return population
+
+
+def particle_swarm_optimization(
+
+) -> Population:
